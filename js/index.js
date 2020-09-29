9e310761335f0df97eee66546f46af1251901d8f
Joe L <joseph.lasata@gmail.com>
2020-09-29 17:20:31 -0500

    Added new event triggers

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ navLinks.forEach((element) => {
     element.addEventListener('click', (event) => {
         event.target.textContent = "I don't go anwhere!";
         event.target.style.color = 'coral';
+        event.preventDefault();
     });
 });
 
@@ -47,3 +48,26 @@ contentText.addEventListener('dblclick', (event) => {
     event.target.style.background = 'white';
 })
 
+// document.addEventListener('volumechange', () => {
+//     alert('Am I too loud or too quiet??');
+// })
+
+const buttons = document.querySelectorAll('.btn'); 
+
+buttons.forEach(element => {
+    element.addEventListener('click', () => {
+        const newButton = document.createElement('a'); 
+        newButton.classList.add('btn'); 
+        newButton.textContent = 'Lets Book It!'; 
+        newButton.href = 'https://www.expedia.com';
+        newButton.target = '_blank'; 
+        newButton.style.marginTop = '5%';
+        element.parentElement.append(newButton);
+    });
+})
+
+const contentIMG = document.querySelector('.content-section img'); 
+contentIMG.addEventListener('drag', (event) => {
+    console.log(event);
+    event.target.style.border = '3px solid green';
+})
